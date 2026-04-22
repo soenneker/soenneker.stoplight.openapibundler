b2e408243b45431c2082c605294748159a570a1e
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:43 -0500

    TUnit conversion

--- test/Soenneker.Stoplight.OpenApiBundler.Tests/StoplightOpenApiBundlerTests.cs
+++ test/Soenneker.Stoplight.OpenApiBundler.Tests/StoplightOpenApiBundlerTests.cs
@@ -1,20 +1,19 @@
 using Soenneker.Stoplight.OpenApiBundler.Abstract;
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Stoplight.OpenApiBundler.Tests;
 
-[Collection("Collection")]
-public sealed class StoplightOpenApiBundlerTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public sealed class StoplightOpenApiBundlerTests : HostedUnitTest
 {
     private readonly IStoplightOpenApiBundler _util;
 
-    public StoplightOpenApiBundlerTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public StoplightOpenApiBundlerTests(Host host) : base(host)
     {
         _util = Resolve<IStoplightOpenApiBundler>(true);
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
